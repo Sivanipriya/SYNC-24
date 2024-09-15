2840589ebfb8fcec24de652d119eab9dcebfc0aa
Sivanipriya <sivanipriyasenthilkumar@gmail.com>
2024-09-15 09:14:04 +0530

    added style

--- sync24/src/components/Contact.jsx
+++ sync24/src/components/Contact.jsx
@@ -1,10 +1,11 @@
 // src/components/Contact.jsx
 import React from 'react';
 
+
 const Contact = () => {
     return (
         <div>
-            <h1>Contact Page</h1>
+            <h2>CONTACT US</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam esse consequuntur neque, explicabo eum maxime, eveniet deserunt voluptate quis pariatur quod est ratione, debitis reprehenderit inventore itaque nihil possimus nobis?</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam esse consequuntur neque, explicabo eum maxime, eveniet deserunt voluptate quis pariatur quod est ratione, debitis reprehenderit inventore itaque nihil possimus nobis?</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam esse consequuntur neque, explicabo eum maxime, eveniet deserunt voluptate quis pariatur quod est ratione, debitis reprehenderit inventore itaque nihil possimus nobis?</p>
