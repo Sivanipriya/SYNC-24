a96d10f660ea34f6e02f423694bfcbc99c69a3a4
VISVESSWAR <amvisvesswar2004@gmail.com>
2024-09-16 22:06:13 +0530

    prefinal event commit

--- sync24/src/App.jsx
+++ sync24/src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Events from "./components/Events/Events";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+
 function App() {
   //const [count, setCount] = useState(0)
 
