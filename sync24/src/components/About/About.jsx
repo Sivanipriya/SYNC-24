2a73180fd86be00b8ea4ee6bdb0d690ca428ff77
harinii19 <142516065+harinii19@users.noreply.github.com>
2024-09-21 09:04:06 +0530

    Update About.jsx

--- sync24/src/components/About/About.jsx
+++ sync24/src/components/About/About.jsx
@@ -1,7 +1,8 @@
 // src/components/About.jsx
 import React, { useEffect } from 'react';
 import ScrollReveal from 'scrollreveal';
-import '../styles/About.css';
+// import '../styles/About.css';
+import '../About.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDay, faMedal } from '@fortawesome/free-solid-svg-icons';
 import aboutImage from '../assets/images/about.png';
